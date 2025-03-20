842df8adb3e7b179516f691fa24406a9f7153037
Seu Nome <seu.email@example.com>
2025-03-20 12:50:47 -0300

    Começando estudo de POO

---
+++ 12_POO/2_instanciando_objetos/index.php
@@ -0,0 +1,8 @@
+<?php
+    class User {
+
+    }
+
+    $peixe = new User;
+    $jao = new User;
+    $cleiton = new User;
---
+++ 12_POO/3_exercicio_52/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Car{
+
+    }
+
+    $bmw = new Car;
+    $gol = new Car;
+    $audi = new Car;
